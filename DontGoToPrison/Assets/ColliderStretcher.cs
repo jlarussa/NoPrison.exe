380fd536f97a4ba5ac89fa655d293afcff427008
TURBINE\jlarussa <james.larussa@gmail.com>
2018-03-03 11:40:54 -0500

    Stretching the collioder to fit the screen

---
+++ DontGoToPrison/Assets/ColliderStretcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderStretcher : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start()
+  {
+    BoxCollider collider = GetComponent<BoxCollider>();
+    RectTransform rect = ( RectTransform )transform;
+    collider.size = new Vector3( rect.rect.width, rect.rect.height, 1 );
+  }
+}
