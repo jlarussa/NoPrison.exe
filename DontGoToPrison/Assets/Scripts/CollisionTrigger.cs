01676b563fe0661d7af3106477ea16496b0a23b9
TURBINE\jlarussa <james.larussa@gmail.com>
2018-03-03 15:30:07 -0500

    GameState

---
+++ DontGoToPrison/Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+  [SerializeField]
+  private GameController.TriggerType triggerType = GameController.TriggerType.Lose;
+  [SerializeField]
+  private string targetTag = string.Empty;
+
+  private void OnTriggerEnter( Collider other )
+  {
+    if ( !string.IsNullOrEmpty( targetTag ) && other.gameObject.tag == targetTag )
+    {
+      GameController.Current.triggerEvent( triggerType );
+    }
+  }
+}
---
+++ DontGoToPrison/Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+  public enum TriggerType
+  {
+    Lose,
+    Win,
+  }
+  public static GameController Current
+  {
+    get;
+    private set;
+  }
+  private Dictionary<TriggerType, Action> triggeredEventList;
+
+  void Awake()
+  {
+    //Mark ourself Don't destroy on load.
+    //singleton may be dumb, but we'll see
+    DontDestroyOnLoad( gameObject );
+    if ( Current == null )
+    {
+      Current = this;
+    }
+    triggeredEventList = new Dictionary<TriggerType, Action>();
+    triggeredEventList[ TriggerType.Lose ] = LoseGame;
+  }
+
+  public void triggerEvent( TriggerType trigger )
+  {
+    if ( triggeredEventList[ trigger ] != null )
+    {
+      triggeredEventList[ trigger ].Invoke();
+    }
+  }
+
+  private void LoseGame()
+  {
+    SceneManager.LoadScene( "MainMenu" );
+  }
+}
