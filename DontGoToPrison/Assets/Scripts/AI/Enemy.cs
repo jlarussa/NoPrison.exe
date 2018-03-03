27df0541efb49b57cd58fb295e450cf6920de601
MikeLyman <MikeMakesApps@gmail.com>
2018-03-02 21:56:54 -0500

    more enemy updates

--- DontGoToPrison/Assets/Scripts/AI/Enemy.cs
+++ DontGoToPrison/Assets/Scripts/AI/Enemy.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour {
 
+	[SerializeField]
+	private Transform goal = null;
+
+	private NavMeshAgent agent = null;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
