2dedba9c23e722bedd154f694c9a11ada256a028
TURBINE\jlarussa <james.larussa@gmail.com>
2018-03-03 18:55:54 -0500

    Adding basics for hotspots and damage

--- DontGoToPrison/Assets/Scripts/AI/Enemy.cs
+++ DontGoToPrison/Assets/Scripts/AI/Enemy.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 [RequireComponent( typeof( UnityEngine.AI.NavMeshAgent ) )]
 public class Enemy : MonoBehaviour
 {
+  public enum DamageSource
+  {
+    hotspot,
+    turret
+  }
 
   [SerializeField]
   private int maxHealth = 100;
 
+  [SerializeField]
+  private List<DamageSource> immunityList = new List<DamageSource>();
+
+  [SerializeField]
   private int currentHealth = 100;
   private void Awake()
   {
     currentHealth = maxHealth;
   }
 
-  public void UpdateHealth( int change )
+  public void UpdateHealth( int change, DamageSource source )
   {
-    currentHealth = Mathf.Max( 0, Mathf.Min( maxHealth, change ) );
+    if ( !immunityList.Contains( source ) )
+    {
+      currentHealth = Mathf.Max( 0, Mathf.Min( maxHealth, change ) );
+    }
     if ( currentHealth == 0 )
     {
       GameObject.Destroy( gameObject );
--- DontGoToPrison/Assets/Scripts/AI/HotSpot.cs
+++ DontGoToPrison/Assets/Scripts/AI/HotSpot.cs
@@ -4,14 +4,51 @@ using UnityEngine;
 
 public class HotSpot : MonoBehaviour
 {
+  private static string enemyTag = "Enemy";
+  [SerializeField]
+  private float rate = 1f;
+  [SerializeField]
+  private int damage = 1;
 
-  void Start()
+  bool isRunning = false;
+
+  private List<Collider> enemies = new List<Collider>();
+  private void OnTriggerEnter( Collider other )
   {
-
+    if ( other.gameObject.tag == enemyTag )
+    {
+      enemies.Add( other );
+      if ( !isRunning )
+      {
+        StartCoroutine( DamageEnemies() );
+      }
+    }
   }
-
-  void Update()
+  private void OnTriggerExit( Collider other )
   {
-
+    if ( other.gameObject.tag == enemyTag )
+    {
+      enemies.Remove( other );
+    }
+  }
+  private IEnumerator DamageEnemies()
+  {
+    if ( !isRunning )
+    {
+      yield return new WaitForEndOfFrame();
+      isRunning = true;
+      var waitTime = new WaitForSeconds( rate );
+      Enemy enemyScript;
+      while ( enemies.Count > 0 )
+      {
+        foreach ( Collider enemy in enemies )
+        {
+          enemyScript = enemy.gameObject.GetComponent<Enemy>();
+          enemyScript.UpdateHealth( -damage, Enemy.DamageSource.hotspot );
+        }
+        yield return waitTime;
+      }
+      isRunning = false;
+    }
   }
 }
