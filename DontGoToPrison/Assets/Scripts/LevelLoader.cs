5d74d207b6cea4e2e985e9774d2dfa4674eeb83a
TURBINE\jlarussa <james.larussa@gmail.com>
2018-03-03 14:07:51 -0500

    Added Level Loading to the game

---
+++ DontGoToPrison/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+  [SerializeField]
+  private string sceneName = string.Empty;
+
+  public void LoadLevel()
+  {
+    if ( sceneName != string.Empty )
+    {
+      SceneManager.LoadScene( sceneName );
+    }
+  }
+}
