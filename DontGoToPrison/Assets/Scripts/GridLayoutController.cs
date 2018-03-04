78b1ca3b38f03f5cc2e43ebbb26ecf58e025c208
MikeLyman <MikeMakesApps@gmail.com>
2018-03-03 22:45:12 -0500

    Getting lazy so I just zoomed the camera in more

---
+++ DontGoToPrison/Assets/Scripts/GridLayoutController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridLayoutController : MonoBehaviour 
+{
+	[SerializeField]
+	private Vector2 gridDimensions = new Vector2( 40, 30 );
+
+	[SerializeField]
+	private Material gridShader = null;
+
+	private AstarPath pathing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
