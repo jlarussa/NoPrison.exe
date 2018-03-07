ba9ed4bd72e56b4384b8f63be3cc43cc15fb17fc
MikeLyman <MikeMakesApps@gmail.com>
2018-03-07 12:30:01 -0500

    Fixing bug where you can cheat the anti cheating system

--- DontGoToPrison/Assets/Scripts/RemainingObjectsDisplay.cs
+++ DontGoToPrison/Assets/Scripts/RemainingObjectsDisplay.cs
@@ -14,6 +14,8 @@ public class RemainingObjectsDisplay : MonoBehaviour
   [SerializeField]
   private Button button;
 
+  private bool isValid = true;
+
   void Awake()
   {
     remainingText = GetComponent<Text>();
@@ -29,18 +31,24 @@ public class RemainingObjectsDisplay : MonoBehaviour
   // adding to the hackniess! Q.Q
   public void PlayPressed()
   {
-    PhaseMaster.Current.BeginPlayPhase();
+    if ( isValid )
+    {
+      PhaseMaster.Current.BeginPlayPhase();
+    }
   }
 
   void OnPathValidityUpdated( object sender, IsValidPathEventArgs args )
   {
     // TODO: change the color of the button to blue / red to match the validator path?
     button.interactable = args.isValidPath;
+    isValid = args.isValidPath;
     remainingText.color = button.interactable ? button.colors.normalColor : button.colors.disabledColor;
   }
 
   void OnRemainingObjectsUpdated( object sender, RemainingObjectsChangedArgs args )
   {
+    // set the path validity to false until the path is re-evaluated
+    isValid = false;
     remainingText.text = string.Format( "Remaining: {0}", args.remainingObjects );
   }
 }
