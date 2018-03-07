04c727813a3ac40ec4211086baf626399a7d6cbc
TURBINE\jlarussa <james.larussa@gmail.com>
2018-03-06 22:28:25 -0500

    All the exposition, a little CRT monitor background, a script to type the words in a text component slowly across the screen

--- DontGoToPrison/Assets/Scripts/StartScene.cs
+++ DontGoToPrison/Assets/Scripts/StartScene.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartScene : MonoBehaviour
 {
-
+  [SerializeField]
+  private SlowTextTyper introStory = null;
   // Use this for initialization
   void Start()
   {
+    if ( introStory != null )
+    {
+      introStory.FinishedTypingEventHandler += WaitForIntro;
+    }
+    else
+    {
+      SceneManager.LoadScene( "MainMenu" );
+    }
+  }
+
+  private void WaitForIntro( object sender, EventArgs args )
+  {
     SceneManager.LoadScene( "MainMenu" );
+
   }
 }
---
+++ DontGoToPrison/Assets/Scripts/UI/SlowTextTyper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent( typeof( Text ) )]
+public class SlowTextTyper : MonoBehaviour
+{
+  public EventHandler FinishedTypingEventHandler;
+
+  [SerializeField]
+  private float speed = 0.5f;
+  [SerializeField]
+  private float punctuationSpeed = 0.5f;
+
+  [SerializeField]
+  private string inputText = string.Empty;
+
+  private Text textField = null;
+
+  private char[] punctuation = { '!', '.', '?' };
+
+  // Use this for initialization
+  void Start()
+  {
+    textField = GetComponent<Text>();
+    if ( string.IsNullOrEmpty( inputText ) )
+    {
+      return;
+    }
+
+    StartCoroutine( SlowTextDisplay( inputText ) );
+  }
+
+  private IEnumerator SlowTextDisplay( string input )
+  {
+    var waitTime = new WaitForSeconds( speed );
+    var punctuationWait = new WaitForSeconds( punctuationSpeed );
+    textField.text = string.Empty;
+    foreach ( char letter in input )
+    {
+      textField.text += letter;
+      if ( punctuation.Contains( letter ) )
+      {
+        yield return punctuationWait;
+        continue;
+      }
+      yield return waitTime;
+
+    }
+    if ( FinishedTypingEventHandler != null )
+    {
+      FinishedTypingEventHandler.Invoke( this, null );
+    }
+  }
+}
