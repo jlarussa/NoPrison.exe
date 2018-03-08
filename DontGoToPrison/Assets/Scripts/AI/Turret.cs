c2a14c398cb88bb9c54a7ce53c783ef4de9a0dfc
jlarussa <james.larussa@gmail.com>
2018-03-08 17:52:48 -0500

    Starting the FTUE. WIP, needs more shit when I get home tonight

--- DontGoToPrison/Assets/Scripts/AI/Turret.cs
+++ DontGoToPrison/Assets/Scripts/AI/Turret.cs
@@ -3,92 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class Turret : MonoBehaviour 
+public class Turret : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject enemyParent = null;
+  [SerializeField]
+  private GameObject enemyParent = null;
 
-	[SerializeField]
-	private float targetInterval = 0.25f;
+  [SerializeField]
+  private float targetInterval = 0.25f;
 
-	[SerializeField]
-	private GameObject bullet = null;
+  [SerializeField]
+  private GameObject bullet = null;
 
-	[SerializeField]
-	private float bulletSpeed = 12f;
+  [SerializeField]
+  private float bulletSpeed = 12f;
 
-	private Transform target = null;
+  private Transform target = null;
 
-	// Use this for initialization
-	void Start () 
-	{
-		if ( enemyParent == null )
-		{
-			enemyParent = GameObject.Find( "Enemy Parent" );
-		}	
+  // Use this for initialization
+  void Start()
+  {
+    if (enemyParent == null)
+    {
+      enemyParent = PlacementTracker.Current.EnemyParent;
+    }
 
-		if ( bullet == null )
-		{
-			Debug.LogError( "Error: turret needs a bullet prefab" );
-		}
-	}
+    if (bullet == null)
+    {
+      Debug.LogError("Error: turret needs a bullet prefab");
+    }
+  }
 
-	public void StartShooting()
-	{
-		StartCoroutine( FindTargetCoroutine() );
-	}
-	
-	private IEnumerator FindTargetCoroutine()
-	{
-		while ( true )
-		{
-			Transform child;
-			Transform bestTarget = null;
-			float closestDistance = Mathf.Infinity;
-			for( int i = 0; i < enemyParent.transform.childCount; i++ )
-			{
-				child = enemyParent.transform.GetChild( i );
-				float distance = Vector3.Distance( transform.position, child.position );
+  public void StartShooting()
+  {
+    StartCoroutine(FindTargetCoroutine());
+  }
 
-				// TODO: determine line of sight before deciding on target
+  private IEnumerator FindTargetCoroutine()
+  {
+    while (true)
+    {
+      Transform child;
+      Transform bestTarget = null;
+      float closestDistance = Mathf.Infinity;
+      for (int i = 0; i < enemyParent.transform.childCount; i++)
+      {
+        child = enemyParent.transform.GetChild(i);
+        float distance = Vector3.Distance(transform.position, child.position);
 
-				if ( distance < closestDistance )
-				{
-					closestDistance = distance;
-					bestTarget = child;
-				}
-			}
-			
-			target = bestTarget;
+        // TODO: determine line of sight before deciding on target
 
-			ShootAtTarget();
+        if (distance < closestDistance)
+        {
+          closestDistance = distance;
+          bestTarget = child;
+        }
+      }
 
-			yield return new WaitForSeconds( targetInterval );
-		}
-	}
+      target = bestTarget;
 
-	private void ShootAtTarget()
-	{
-		var spawnedBullet = Instantiate( bullet, transform.position, Quaternion.identity );
-		Vector3 velocityDirection = target.position - transform.position;
-		velocityDirection.Normalize();
+      ShootAtTarget();
 
-		Vector3 bulletSpawnPosition = transform.position + velocityDirection * 0.5f;
+      yield return new WaitForSeconds(targetInterval);
+    }
+  }
 
-		// Try to lead the target via it's velocity?
-		Vector3 targetVelocity = target.GetComponent<AIPath>().velocity;
-		targetVelocity.Normalize();
-		
-		// Note: this is super half-assed and cheesy. To do it for real, 
-		// we should also take into account the distance from target.... but this is good enough
-		velocityDirection = ( velocityDirection + targetVelocity * 0.2f );
+  private void ShootAtTarget()
+  {
+    var spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+    Vector3 velocityDirection = target.position - transform.position;
+    velocityDirection.Normalize();
 
-		spawnedBullet.transform.position = bulletSpawnPosition;
-		spawnedBullet.GetComponent<Rigidbody>().velocity = velocityDirection * bulletSpeed;
-	}
+    Vector3 bulletSpawnPosition = transform.position + velocityDirection * 0.5f;
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Try to lead the target via it's velocity?
+    Vector3 targetVelocity = target.GetComponent<AIPath>().velocity;
+    targetVelocity.Normalize();
+
+    // Note: this is super half-assed and cheesy. To do it for real, 
+    // we should also take into account the distance from target.... but this is good enough
+    velocityDirection = (velocityDirection + targetVelocity * 0.2f);
+
+    spawnedBullet.transform.position = bulletSpawnPosition;
+    spawnedBullet.GetComponent<Rigidbody>().velocity = velocityDirection * bulletSpeed;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
--- DontGoToPrison/Assets/Scripts/PlacementTracker.cs
+++ DontGoToPrison/Assets/Scripts/PlacementTracker.cs
@@ -13,6 +13,9 @@ public class PlacementTracker : Singleton<PlacementTracker>
   [SerializeField]
   private List<string> NonErasableObjects = new List<string>();
 
+  [SerializeField]
+  public GameObject EnemyParent = null;
+
   private GridGraph graph;
 
   // This is a set of all locations the player has put stuff.
@@ -27,25 +30,25 @@ public class PlacementTracker : Singleton<PlacementTracker>
   /// </summary>
   /// <param name="tileLocation"></param>
   /// <param name="objectAtTile"></param>
-  public void RegisterTile( Vector3 tileLocation, GameObject objectAtTile = null )
+  public void RegisterTile(Vector3 tileLocation, GameObject objectAtTile = null)
   {
-    trackedLocations[ GetSnapToGridPosition( tileLocation ) ] = objectAtTile;
+    trackedLocations[GetSnapToGridPosition(tileLocation)] = objectAtTile;
   }
 
   /// <summary>
   /// When a tile is pressed, it tells the placement tracker. Then the placement tracker does the correct action.
   /// </summary>
   /// <param name="tileLocation"></param>
-  public void TilePressed( Vector3 tileLocation )
+  public void TilePressed(Vector3 tileLocation)
   {
-    Vector3 snappedLocation = GetSnapToGridPosition( tileLocation );
-    if ( isEraserMode )
+    Vector3 snappedLocation = GetSnapToGridPosition(tileLocation);
+    if (isEraserMode)
     {
-      Erase( snappedLocation );
+      Erase(snappedLocation);
       return;
     }
 
-    AddNewTrackedObject( snappedLocation, prefab );
+    AddNewTrackedObject(snappedLocation, prefab);
   }
 
   public void SelectEraser()
@@ -53,11 +56,11 @@ public class PlacementTracker : Singleton<PlacementTracker>
     isEraserMode = true;
   }
 
-  private void Erase( Vector3 snappedLocation )
+  private void Erase(Vector3 snappedLocation)
   {
-    if ( !trackedLocations.ContainsKey( snappedLocation )
-      || trackedLocations[ snappedLocation ] == null
-      || NonErasableObjects.Contains( trackedLocations[ snappedLocation ].tag ) )
+    if (!trackedLocations.ContainsKey(snappedLocation)
+      || trackedLocations[snappedLocation] == null
+      || NonErasableObjects.Contains(trackedLocations[snappedLocation].tag))
     {
       return;
     }
@@ -65,33 +68,33 @@ public class PlacementTracker : Singleton<PlacementTracker>
     // get the object and move it far away so it can be destroyed at Unity's own pace.
     // if we leave it here and destroy it, it won't dissapear immediately so rebuilding the pathfinding grid
     // will still think it is there.
-    PlacementAmountConfig.Current.AdjustPrefabAmount( trackedLocations[ snappedLocation ], -1 );
+    PlacementAmountConfig.Current.AdjustPrefabAmount(trackedLocations[snappedLocation], -1);
 
-    trackedLocations[ snappedLocation ].transform.position = new Vector3( 100000, 100000, 100000 );
-    Destroy( trackedLocations[ snappedLocation ] );
-    trackedLocations.Remove( snappedLocation );
+    trackedLocations[snappedLocation].transform.position = new Vector3(100000, 100000, 100000);
+    Destroy(trackedLocations[snappedLocation]);
+    trackedLocations.Remove(snappedLocation);
 
     RebuildGrid();
 
     return;
   }
 
-  public bool AddNewTrackedObject( Vector3 snappedLocation, GameObject prefab, Transform parentTransform = null )
+  public bool AddNewTrackedObject(Vector3 snappedLocation, GameObject prefab, Transform parentTransform = null)
   {
-    if ( trackedLocations.ContainsKey( snappedLocation ) )
+    if (trackedLocations.ContainsKey(snappedLocation))
     {
       return false;
     }
 
-    if ( !PlacementAmountConfig.Current.PrefabHasUsesLeft( prefab ) )
+    if (!PlacementAmountConfig.Current.PrefabHasUsesLeft(prefab))
     {
       return false;
     }
 
-    PlacementAmountConfig.Current.AdjustPrefabAmount( prefab, 1 );
+    PlacementAmountConfig.Current.AdjustPrefabAmount(prefab, 1);
 
-    GameObject obj = GameObject.Instantiate( prefab, snappedLocation, Quaternion.identity, parentTransform );
-    trackedLocations.Add( snappedLocation, obj );
+    GameObject obj = GameObject.Instantiate(prefab, snappedLocation, Quaternion.identity, parentTransform);
+    trackedLocations.Add(snappedLocation, obj);
 
     RebuildGrid();
 
@@ -103,20 +106,20 @@ public class PlacementTracker : Singleton<PlacementTracker>
     AstarPath.active.Scan();
   }
 
-  private Vector3 GetSnapToGridPosition( Vector3 unsnappedPosition )
+  private Vector3 GetSnapToGridPosition(Vector3 unsnappedPosition)
   {
-    if ( graph == null )
+    if (graph == null)
     {
-      graph = AstarPath.active.graphs[ 0 ] as GridGraph;
+      graph = AstarPath.active.graphs[0] as GridGraph;
     }
 
     float nodeSize = 1;
-    float snapX = Mathf.Round( unsnappedPosition.x / nodeSize ) * nodeSize - 0.5f;
-    float snapZ = Mathf.Round( unsnappedPosition.z / nodeSize ) * nodeSize - 0.5f;
-    return new Vector3( snapX, 0, snapZ );
+    float snapX = Mathf.Round(unsnappedPosition.x / nodeSize) * nodeSize - 0.5f;
+    float snapZ = Mathf.Round(unsnappedPosition.z / nodeSize) * nodeSize - 0.5f;
+    return new Vector3(snapX, 0, snapZ);
   }
 
-  public void SelectPrefab( GameObject selectedPrefab )
+  public void SelectPrefab(GameObject selectedPrefab)
   {
     prefab = selectedPrefab;
     isEraserMode = false;
