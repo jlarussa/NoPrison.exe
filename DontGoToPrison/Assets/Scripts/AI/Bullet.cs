9396a800b356dfc36d8011eecac5be1146aca000
MikeLyman <MikeMakesApps@gmail.com>
2018-03-07 21:13:06 -0500

    Added turret to test scene. No button to spawn it yet though.

---
+++ DontGoToPrison/Assets/Scripts/AI/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+	[SerializeField]
+	private float bulletExpirationTime = 3.0f;
+
+	[SerializeField]
+	private int damage = 15;
+
+	private const string enemyTag = "Enemy";
+
+	private const string wallTag = "Wall";
+
+	private const string borderTag = "Border";
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine( DestroyCoroutine() );	
+	}
+
+	private IEnumerator DestroyCoroutine()
+	{
+		yield return new WaitForSeconds( bulletExpirationTime );
+		Destroy( this.gameObject );
+	}
+
+	private void OnTriggerEnter( Collider other )
+  {
+    if ( other.gameObject.tag == enemyTag  )
+    {
+			Destroy( this.gameObject );
+			other.gameObject.GetComponent<Enemy>().UpdateHealth( -damage, Enemy.DamageSource.turret );
+			return;
+    }
+
+		if ( other.gameObject.tag == wallTag || other.gameObject.tag == borderTag )
+		{
+			Destroy( this.gameObject );
+		}
+  }
+
+}
---
+++ DontGoToPrison/Assets/Scripts/PhaseControl/TurretPhaseToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+[RequireComponent( typeof( Turret ) ) ]
+public class TurretPhaseToggle : PhaseToggle 
+{
+
+	private Turret turret;
+
+	protected override void ToggleOnStart()
+	{
+		turret = GetComponent<Turret>();
+		PhaseMaster.Current.onPhaseChange += OnPhaseChange;
+	}
+
+	private void OnPhaseChange( Phase phase )
+	{
+		turret.StartShooting();
+	}
+}
