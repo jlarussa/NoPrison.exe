506ce2333873462c2cab9edf229dd982b0ab11c2
jlarussa <james.larussa@gmail.com>
2018-03-08 22:41:41 -0500

    FTUE level 1 is done

---
+++ DontGoToPrison/Assets/Scripts/UI/FTUESelfDestruct.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTUESelfDestruct : SelfDestruct
+{
+  [SerializeField]
+  public GameObject nextPanel = null;
+
+  void Start()
+  {
+    if (nextPanel != null)
+    {
+      nextPanel.SetActive(false);
+    }
+  }
+  public override void Destroy()
+  {
+    if (nextPanel != null)
+    {
+      nextPanel.SetActive(true);
+    }
+    base.Destroy();
+  }
+}
